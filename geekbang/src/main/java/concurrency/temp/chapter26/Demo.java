c7cdf7bbde468be260637f20a08d7164d893962c
oiuio <oiuio.cc@qq.com>
2019-06-28 10:15:59 +0800

    更新一部分

--- geekbang/src/main/java/concurrency/temp/chapter26/Demo.java
+++ geekbang/src/main/java/concurrency/temp/chapter26/Demo.java
@@ -83,7 +83,9 @@ class 模拟MapReduce统计单词数量 {
     public static void main(String[] args) {
         String[] fc = {"hello world", "hello me", "hello fork", "hello join", "fork join in world"};
         ForkJoinPool fjp = new ForkJoinPool(3);
-
+        MR mr = new MR(fc, 0, fc.length);
+        Map<String, Long> result = fjp.invoke(mr);
+        result.forEach((k, v) -> System.out.println(k + ":" + v));
     }
 }
 
@@ -99,7 +101,16 @@ class MR extends RecursiveTask<Map<String, Long>> {
 
     @Override
     protected Map<String, Long> compute() {
-        return null;
+
+        if (end - start == 1) {
+            return calc(fc[start]);
+        } else {
+            int mid = (start + end) / 2;
+            MR mr1 = new MR(fc, start, mid);
+            mr1.fork();
+            MR mr2 = new MR(fc, mid, end);
+            return merge(mr2.compute(), mr1.join());
+        }
     }
 
     //合并结果
@@ -129,6 +140,7 @@ class MR extends RecursiveTask<Map<String, Long>> {
                 result.put(w, 1L);
             }
         }
+
         return result;
     }
 
